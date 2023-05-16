3f9a2b0365c166ab1e6415afadde8da7279b8a61
Serhat <akifserhatgunes@gmail.com>
2023-05-16 16:15:41 +0300

    gmmmm

--- main.tf
+++ main.tf
@@ -151,10 +151,10 @@ resource "aws_security_group" "tf-sec-gr" {
 
 # }
 
-output "controlnodeip" {
-  value = aws_instance.control_node.public_ip
-}
+# output "controlnodeip" {
+#   value = aws_instance.control_node.public_ip
+# }
 
-output "privates" {
-  value = aws_instance.control_node.*.private_ip
-}
+# output "privates" {
+#   value = aws_instance.control_node.*.private_ip
+# }
